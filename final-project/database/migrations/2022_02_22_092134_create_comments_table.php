8879d7931924aaa44088e1df3831d1e4891f285c
nhutrong11355 <nguyennhutrong69@gmail.com>
2022-02-24 08:47:15 +0700

    add Database

---
+++ final-project/database/migrations/2022_02_22_092134_create_comments_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('homestay_id');
+            $table->enum('status', [0, 1, 2])->comment = "0:normal, 1:locked";
+            $table->integer('parent_id');
+            $table->string('content');
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('homestay_id')->references('id')->on('homestays')->onDelete('cascade');
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
