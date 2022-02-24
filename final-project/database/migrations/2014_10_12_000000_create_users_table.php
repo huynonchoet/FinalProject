bc7275a399ce367a239af59c54f611bfdfa06a21
huynhngocvien <huynhngocvien27@gmail.com>
2022-02-24 15:35:00 +0700

    login and fix migration user

--- final-project/database/migrations/2014_10_12_000000_create_users_table.php
+++ final-project/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,16 +15,15 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('user_name')->unique();
+            $table->string('email')->unique()->nullable();
             $table->string('password');
             $table->string('name')->nullable();
-            $table->string('email')->unique()->nullable();
-            $table->timestamp('email_verified_at')->nullable();
             $table->enum('gender', [0, 1])->nullable()->comment = "0:male,1:female";
             $table->enum('role', [0, 1])->default(0)->comment = '1:admin 0:member';
             $table->string('avatar')->nullable();
             $table->date('birthday')->nullable();
             $table->string('address')->nullable();
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();
--- final-project/resources/views/auth/login.blade.php
+++ final-project/resources/views/auth/login.blade.php
@@ -33,11 +33,12 @@
                                         <h2>Login</h2>
                                     </div>
                                     <div class="content">
-                                        <form id="contact" action="" method="post">
+                                        <form id="contact" action="{{ route('login.post') }}" method="post">
+                                            @csrf
                                             <div class="row">
                                                 <div class="col-md-12 col-sm-12">
                                                     <fieldset>
-                                                        <input name="username" type="text" placeholder="Username">
+                                                        <input name="email" type="text" placeholder="Username">
                                                     </fieldset>
                                                 </div>
                                                 <div class="col-md-12 col-sm-12">
@@ -47,7 +48,8 @@
                                                 </div>
                                                 <div class="col-lg-12">
                                                     <fieldset>
-                                                        <button type="submit" id="form-submit" class="main-button">Login</button>
+                                                        <button type="submit" id="form-submit"
+                                                            class="main-button">Login</button>
                                                     </fieldset>
                                                 </div>
                                             </div>
