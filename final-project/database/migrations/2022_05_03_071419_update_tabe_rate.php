2e92807213c311da5e953789eb6af6baed9894e5
huynhngocvien <huynhngocvien27@gmail.com>
2022-05-03 14:23:09 +0700

    fix icon3

---
+++ final-project/database/migrations/2022_05_03_071419_update_tabe_rate.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateTabeRate extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('rates', function (Blueprint $table) {
+            $table->unsignedBigInteger('bookingDetails_id')->notNull();
+            $table->foreign('bookingDetails_id')->references('id')->on('booking_details')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('Rate', function (Blueprint $table) {
+            //
+        });
+    }
+}
